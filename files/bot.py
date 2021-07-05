d766eb06aafd10100b55fb62679bfbb58e940da4
me <steve10.live@gmail.com>
2021-07-05 21:41:27 +0300

    Initial commit

---
+++ files/bot.py
@@ -0,0 +1,67 @@
+import os
+import asyncio
+import objects
+import _thread
+from time import sleep
+from objects import GoogleDrive
+from telethon.sync import TelegramClient, events
+from datetime import datetime, timezone, timedelta
+# =================================================================================================================
+stamp1 = objects.time_now()
+Auth = objects.AuthCentre(ID_DEV=-1001312302092, TOKEN=os.environ['TOKEN'])
+
+
+def sessions_creation():
+    objects.environmental_files()
+    drive_client = GoogleDrive('google.json')
+    for file in drive_client.files():
+        user_session = f"{os.environ['session']}.session"
+        if file['name'] == user_session:
+            drive_client.download_file(file['id'], f'{user_session}')
+            Auth.dev.printer(f'Скачали сессию Telegram: {user_session}')
+
+
+sessions_creation()
+tz = timezone(timedelta(hours=3))
+# =================================================================================================================
+Auth.dev.start(stamp1)
+
+
+def thread():
+    try:
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        client = TelegramClient(os.environ['session'], int(os.environ['api_id']), os.environ['api_hash']).start()
+        with client:
+            Auth.dev.printer(f"Сессия в работе: {os.environ['session']}")
+
+            @client.on(events.NewMessage(chats=os.environ['chat']))
+            async def response_user_update(response):
+                await client.forward_messages(-1001532518410, response.message)
+
+            client.run_until_disconnected()
+    except IndexError and Exception:
+        Auth.dev.thread_except()
+
+
+def auto_reboot():
+    reboot = None
+    while True:
+        try:
+            sleep(30)
+            date = datetime.now(tz)
+            if date.strftime('%H') == '01' and date.strftime('%M') == '59':
+                reboot = True
+                while date.strftime('%M') == '59':
+                    sleep(1)
+                    date = datetime.now(tz)
+            if reboot:
+                reboot = None
+                text, _ = Auth.logs.reboot()
+                Auth.dev.printer(text)
+        except IndexError and Exception:
+            Auth.dev.thread_except()
+
+
+if __name__ == '__main__':
+    _thread.start_new_thread(auto_reboot, ())
+    thread()
